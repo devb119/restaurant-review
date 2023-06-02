3a09767ef9eb01226c922a75e5b2b6601675388a
ashley.nguyen <ashley.nguyen@avepoint.com>
2023-06-02 10:51:51 +0700

    add user model and enum

--- src/models/user_coupon.tsx
+++ src/models/user_coupon.tsx
@@ -1,9 +1,11 @@
+import { UserCouponStatus } from "./enum";
+
 export default interface User_coupon {
     user_id?: string;
     coupon_id: string;
     amount: number;
     got_at?: Date;
-    status: number;
+    status: UserCouponStatus;
 }
 
 //viet ham validation cho data o day
