72bc89844f5a3c8ff13d27cf8dab1f1263013b9a
ashley.nguyen <ashley.nguyen@avepoint.com>
2023-06-06 23:34:23 +0700

    use api getRestaurant

--- src/pages/restaurant_lists/Restaurants.tsx
+++ src/pages/restaurant_lists/Restaurants.tsx
@@ -1,35 +1,38 @@
-import React from 'react'
-import { RestaurantCard, Section } from '../../components'
-import {getRestaurantsByName } from '../../services/RestaurantApi';
-
+import React from "react";
+import { RestaurantCard, Section } from "../../components";
+import { getRestaurantsByName } from "../../services/RestaurantApi";
 
 function Restaurants() {
   const [loading, setLoading] = React.useState(true);
   const [restaurantLists, setRestaurantLists] = React.useState<any>([]);
   React.useEffect(() => {
-    getRestaurantsByName("").then((res) => setRestaurantLists(res.slice(0,5))).catch((err)=> console.log(err)).then(()=>{setLoading(false)});
-  
-  },[])
+    getRestaurantsByName("")
+      .then((res) => setRestaurantLists(res.slice(0, 5)))
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-    return (
-      <Section title="レストラン">
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <div className="flex">
-            {restaurantLists.map((i: any, index: number) => (
-              <RestaurantCard
-                title={i.name}
-                rating={i.rating}
-                address={i.address}
-                imageUrl={i.image}
-                key={index}
-              ></RestaurantCard>
-            ))}
-          </div>
-        )}
-      </Section>
-    );
+  return (
+    <Section title="レストラン">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="flex">
+          {restaurantLists.map((i: any, index: number) => (
+            <RestaurantCard
+              title={i.name}
+              rating={i.rating}
+              address={i.address}
+              imageUrl={i.image}
+              key={index}
+            ></RestaurantCard>
+          ))}
+        </div>
+      )}
+    </Section>
+  );
 }
 
-export default Restaurants
+export default Restaurants;
