ce6a1df9953ab8336f547032058a3aacc8164783
hoanganhnguyen0710 <vipboy69798999@gmail.com>
2023-06-30 00:47:33 +0700

    add food cmt image

--- src/components/MyComment.tsx
+++ src/components/MyComment.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { SlPaperPlane } from "react-icons/sl";
 import { BiCamera } from "react-icons/bi";
 import Rate from "./Rate";
@@ -8,38 +8,69 @@ import FoodReview from "../models/food_reviews";
 import { createNewFoodReview } from "../services/FoodReviewApi";
 import { FoodReviewContext } from "../pages/food_details/FoodReviewSection";
 import { Link } from "react-router-dom";
+import { imageUploader } from "../services/ImageUploader";
 
 interface Props {
   type: string;
   id: string;
 }
 const MyComment = (props: Props) => {
+  const inputFile = useRef<HTMLInputElement>(null);
   const [rating, setRating] = useState(0);
   const [content, setContent] = useState<string>("");
   const [message, setMessage] = useState<string>("");
   const { reviewList, setReviewList } = useContext(FoodReviewContext);
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
 
   const user = useSelector((state: RootState) => state.user.user);
 
+  const uploadPicture = () => {
+    if (!inputFile.current) return;
+    inputFile.current.click();
+  };
 
   const handleSubmit = () => {
     if (content.length === 0 || rating === 0) {
       setMessage("項目をすべて入力してください");
       return;
     } else {
+      if(selectedImage){
+      const uploadImg = imageUploader(user?.email + '/reviews/', selectedImage);
+      Promise.all([uploadImg]).then((result)=>{
+        // console.log(result);
+        const review: FoodReview = {
+          food_id: props.id,
+          star: rating,
+          about_price: content,
+          about_decoration: content,
+          other: content,
+          image_url: String(result),
+        };
+        createNewFoodReview(review);
+        setMessage("追加できました。");
+        setReviewList([...reviewList, review]);
+        setContent("");
+        setSelectedImage(null);
+        setRating(0);
+      })
+    }
+    else {
       const review: FoodReview = {
         food_id: props.id,
         star: rating,
         about_price: content,
         about_decoration: content,
         other: content,
+        image_url: '',
       };
       createNewFoodReview(review);
       setMessage("追加できました。");
       setReviewList([...reviewList, review]);
       setContent("");
+      setSelectedImage(null);
       setRating(0);
     }
+  }
   };
 
   return (
@@ -71,9 +102,37 @@ const MyComment = (props: Props) => {
               name="content"
               className="pl-3 pt-3 border border-slate-300 rounded-lg bg-transparent w-full pr-16 h-1/2"
               onChange={(e) => setContent(e.target.value)}
+              value={content}
             ></textarea>
+            {props.type === "main" ? (
+              // <BiCamera className="text-5xl mb-10" />
+              <div className="flex items-center mb-5">
+                <button
+                  className="text-center border-slate-300 rounded-lg text-green-400 font-black"
+                  onClick={uploadPicture}
+                >
+                  <BiCamera className="text-5xl" />
+                </button>
+                <input
+                  type={"file"}
+                  style={{ display: "none" }}
+                  ref={inputFile}
+                  onChange={(event) => {
+                    if (!event.target.files) return;
+                    setSelectedImage(event.target.files[0]);
+                  }}
+                  accept="image/gif,image/jpeg,image/jpg,image/png"
+                  name="image"
+                />
+                {selectedImage && (
+                  <div>
+                    {selectedImage.name}{"  "}
+                    <button onClick={() => setSelectedImage(null)}>削除</button>
+                  </div>
+                )}
+              </div>
+            ) : null}
             <div className="text-main">{message}</div>
-            {props.type === "main" ? <BiCamera className="text-5xl mb-10" /> : null}
           </div>
           <SlPaperPlane
             onClick={handleSubmit}
